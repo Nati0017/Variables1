6bb25d4cb91a1ffadc6f92d91598faf27c5efea8
Natasha Pérez <165829944+Nati0017@users.noreply.github.com>
2024-06-20 16:52:35 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Buenas tardes, bienvenido a tu carrito de compras!! ")
+shopping_list = []
+articulo_1 = input("Agregue el primer elemento: ")
+articulo_2 = input("Agregue el segundo elemento: ")
+articulo_3 = input("Agregue el tercer elemento: ")
+print(shopping_list)
+print("Elementos agregados al carrrito: ")
+shopping_list.append(articulo_1)
+shopping_list.append(articulo_2)
+shopping_list.append(articulo_3)
+print(shopping_list) 
